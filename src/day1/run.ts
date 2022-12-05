6e1c9c6240827bb0e561e74cee39fb1d42b1e09e
Peter Swallow <peter.swallow@easyjet.com>
2022-12-05 10:22:03 +0000

    Read in day 1 input

--- src/day1/run.ts
+++ src/day1/run.ts
@@ -1,1 +1,18 @@
-console.log('hello day 2');
+console.log('Running day 1');
+
+import fs, { promises as fsPromises } from 'fs';
+
+const input = readInput();
+
+async function readInput() {
+    try
+    {
+        const data = await fsPromises
+            .readFile('./src/day1/input.txt')
+            .catch((err: any) => console.error('failed to read day 1 input file', err));
+        console.log(data);
+    }
+    catch (error){
+        console.log(error);
+    }
+}
